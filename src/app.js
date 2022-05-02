17512942b4af2701bcb6d24702d248230726de43
Koen Verduijn <koen.verduijn@aircall.io>
2022-05-02 15:45:15 +0200

    moved to config-manager

--- src/app.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import HelloWorld from './helloworld';
-
-render(<HelloWorld />, document.getElementById('app-root'));
--- src/helloworld.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class HelloWorld extends Component {
-  render() {
-    return (<h1>Hello World, Aircall Koens config manager is up!</h1>);
-  }
-}
-
-export default HelloWorld;
